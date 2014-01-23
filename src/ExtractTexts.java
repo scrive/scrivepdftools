9073b04d8d48d5f750f16acbc969207587910e9d
Gracjan Polak <gracjan@scrive.com>
2014-01-23 08:58:44 +0100

    Add GNU Affero v3 on each of *.java files

--- src/ExtractTexts.java
+++ src/ExtractTexts.java
@@ -1,16 +1,19 @@
 /*
+ *  Copyright (C) 2014 Scrive AB
  *
- * This program does the following:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * 1. Stamps fields over pdf content.
- * 2. Add pagination marks over pdf content.
- * 3. Appends pdf seal pages created from source data.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
  *
- * Usage:
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *     java -jar pdfseal.jar config.json
- *
- * Where config.json file is a configuration file. For its spec read the code below.
  */
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
