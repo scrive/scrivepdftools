ef213ec010d7c5e8352ebc8a2515a0c5308c194a
skaven <mikolaj.zuzek@queltix.com>
2015-07-25 18:46:35 +0200

    Content stream clean up

--- src/RemoveScriveElements.java
+++ src/RemoveScriveElements.java
@@ -15,31 +15,51 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Stack;
+
+import org.bouncycastle.util.Arrays;
+import org.yaml.snakeyaml.TypeDescription;
 
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.io.RandomAccessSource;
-import com.itextpdf.text.io.RandomAccessSourceFactory;
-import com.itextpdf.text.pdf.PRTokeniser;
-import com.itextpdf.text.pdf.PdfContentParser;
 import com.itextpdf.text.pdf.PdfDictionary;
 import com.itextpdf.text.pdf.PdfName;
-import com.itextpdf.text.pdf.PdfObject;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
-import com.itextpdf.text.pdf.RandomAccessFileOrArray;
+import com.itextpdf.text.pdf.PdfStream;
+
+class RemoveScriveElementsSpec extends YamlSpec
+{
+    public Boolean removeFooter = false;
+    public Boolean removeVerificationPages = false;
+    public Boolean removeBackground = false;
+    public Boolean removeEvidenceAttachments = false;
+
+    static private ArrayList<TypeDescription> td = null;
+
+    static public ArrayList<TypeDescription> getTypeDescriptors() {
+        if (td == null) {
+            td = new ArrayList<TypeDescription>();
+            td.add(new TypeDescription(RemoveScriveElementsSpec.class));
+        }
+        return td;
+    }
+}
 
 public class RemoveScriveElements extends Engine {
 
-    YamlSpec spec = null;
+    RemoveScriveElementsSpec spec = null;
 
     public void Init(InputStream specFile, String inputOverride, String outputOverride) throws IOException {
-        spec = YamlSpec.loadFromStream(specFile, YamlSpec.class);
+        YamlSpec.setTypeDescriptors(RemoveScriveElementsSpec.class, RemoveScriveElementsSpec.getTypeDescriptors());
+        spec = YamlSpec.loadFromStream(specFile, RemoveScriveElementsSpec.class);
         if (inputOverride != null) {
             spec.input = inputOverride;
         }
@@ -63,13 +83,23 @@ public class RemoveScriveElements extends Engine {
         String keep = "";
         for (int i = 1; i <= n; i++) {
             PdfDictionary page = reader.getPageN(i);
-            if (page.contains(AddVerificationPages.scriveTag))
-            	continue; // remove the whole page
+            if (spec.removeVerificationPages && AddVerificationPages.scriveTagVerPage.equals(page.get(AddVerificationPages.scriveTag)))
+                continue; // remove the whole page
             keep = keep.isEmpty() ? String.valueOf(i) : keep + "," + i;
+            
+            PdfDictionary res = page.getAsDict(PdfName.RESOURCES);
+            PdfDictionary xobj = (null == res) ? null : res.getAsDict(PdfName.XOBJECT);
+            if (null != xobj)
+                for (PdfName key: xobj.getKeys()) {
+                    PdfStream x = (PdfStream)reader.getPdfObject(xobj.getAsIndirectObject(key).getNumber());
+                    if ((null == x) || !PdfName.FORM.equals(x.get(PdfName.SUBTYPE)))
+                        continue;
+                }
+            
             byte[] content = reader.getPageContent(i);
-            byte[] content2 = checkContent(content, page.getAsDict(PdfName.RESOURCES));
+            byte[] content2 = checkContent(content, res);
             if (content2 != content)
-            	reader.setPageContent(i,  content2);
+                reader.setPageContent(i,  content2);
         }
         reader.selectPages(keep);
 
