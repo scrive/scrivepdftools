efcdb389bc073bf8a1a618813955e69f76e91957
skaven <mikolaj.zuzek@queltix.com>
2015-08-31 19:19:32 +0200

    Removing footer and background: content + resources

--- src/RemoveScriveElements.java
+++ src/RemoveScriveElements.java
@@ -21,11 +21,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.Stack;
 
-import org.bouncycastle.util.Arrays;
 import org.yaml.snakeyaml.TypeDescription;
 
 import com.itextpdf.text.DocumentException;
@@ -67,7 +68,7 @@ public class RemoveScriveElements extends Engine {
             spec.output = outputOverride;
         }
     }
-    
+
     public void execute(InputStream pdf, OutputStream os) throws IOException, DocumentException {
         if (pdf == null)
             pdf = new FileInputStream(spec.input);
@@ -86,7 +87,7 @@ public class RemoveScriveElements extends Engine {
             if (spec.removeVerificationPages && AddVerificationPages.scriveTagVerPage.equals(page.get(AddVerificationPages.scriveTag)))
                 continue; // remove the whole page
             keep = keep.isEmpty() ? String.valueOf(i) : keep + "," + i;
-            
+
             PdfDictionary res = page.getAsDict(PdfName.RESOURCES);
             PdfDictionary xobj = (null == res) ? null : res.getAsDict(PdfName.XOBJECT);
             if (null != xobj)
@@ -95,7 +96,7 @@ public class RemoveScriveElements extends Engine {
                     if ((null == x) || !PdfName.FORM.equals(x.get(PdfName.SUBTYPE)))
                         continue;
                 }
-            
+
             byte[] content = reader.getPageContent(i);
             byte[] content2 = checkContent(content, res);
             if (content2 != content)
@@ -108,8 +109,153 @@ public class RemoveScriveElements extends Engine {
         pdf.close();
     }
 
-	private byte[] checkContent(byte[] content, PdfDictionary resources) throws IOException {
-		// TODO: find & remove BMC..EMC sections with Scrvie* tags
-		return content;
-	}
+    private interface IByteSearchListener {
+        /**
+         * @param position content stream position
+         */
+
+        /**
+         * Callback for byte content stream linear search
+         * @param tag      found tag
+         * @param tagPos   tag position
+         * @param id       preceding identifier
+         * @param idPos    identifier position
+         */
+        public void onItem(byte[] tag, int tagPos, String id, int idPos);
+    }
+
+    private static class ResFinder implements IByteSearchListener {
+        public static byte[] tag = "Do".getBytes();
+        public Set<String>   refs = new HashSet<String>();
+        public boolean       add = true;
+
+        public void onItem(byte[] tag, int tagPos, String id, int idPos) {
+            if (add) {
+                refs.add(id);
+            } else {
+                refs.remove(id);
+            }
+        }
+    }
+
+    private static class BlockFinder implements IByteSearchListener {
+        public static byte[]   tagBMC = "BMC".getBytes();
+        public static byte[]   tagEMC = "EMC".getBytes();
+        public static String[] toRemove = {"/Scrive:Background", "/Scrive:Footer"};
+
+        public static class BlockIndex {
+            public int start, end;
+            public BlockIndex(int s, int e) {
+                start = s;
+                end = e;
+            }
+            public String toString() {
+                return "{" + start + ":" + end + "}";
+            }
+        }
+
+        private Stack<Integer> open = new Stack<Integer>();
+        public ArrayList<BlockIndex> blocks = new ArrayList<BlockIndex>();
+
+        public int getTotalLength() {
+            int len = 0;
+            for (BlockIndex i: blocks)
+                len += i.end - i.start;
+            return len;
+        }
+
+        private boolean check(String id) {
+            for (String i: toRemove)
+                if (i.equals(id))
+                    return true;
+            return false;
+        }
+
+        public void onItem(byte[] tag, int tagPos, String id, int idPos) {
+            if (tagBMC == tag) {
+                open.push(check(id) ? idPos : -idPos);
+            } else if (tagEMC == tag) {
+                final int start = open.peek().intValue();
+                open.pop();
+                if (start >= 0) {
+                    /**
+                     * Important: remove all blocks enclosed with the current one
+                     */
+                    while (!blocks.isEmpty() && (blocks.get(blocks.size() - 1).end > start))
+                        blocks.remove(blocks.get(blocks.size() - 1));
+                    blocks.add(new BlockIndex(start, tagPos + tag.length));
+                }
+            }
+        }
+    }
+
+    /**
+     *  Drives content search and calls listeners registered for specific tokens.
+     * @param content
+     * @param cb
+     */
+    private void search(byte[] content, Map<byte[], IByteSearchListener> cb) {
+        boolean start = true;
+        int ii = 0, k = 0, l = 0;
+        for (int i = 0; i < content.length; ++i) {
+            if (Character.isWhitespace(content[i])) {
+                if (!start) {
+                    k = ii; l = i - ii;
+                    start = true;
+                }
+            } else if (start) {
+                ii = i;
+                start = false;
+            }
+            for (byte[] c: cb.keySet()) {
+                if (i + c.length > content.length)
+                    continue;
+                int j = 0;
+                while ((j < c.length) && (c[j] == content[i + j]))
+                    ++j;
+                if (j == c.length)
+                    cb.get(c).onItem(c, i, new String(content, k, l), k); // report found item
+            }
+        }
+    }
+
+    private byte[] checkContent(byte[] content, PdfDictionary resources) throws IOException {
+        // Track resource calls (forms and images)
+        ResFinder res = new ResFinder();
+        Map<byte[], IByteSearchListener> cb = new HashMap<byte[], IByteSearchListener>();
+        cb.put(res.tag, res);
+        // Find BMC..EMC blocks
+        BlockFinder blocks = new BlockFinder();
+        cb.put(blocks.tagBMC, blocks);
+        cb.put(blocks.tagEMC, blocks);
+        search(content, cb);
+
+        // Remove BMC..EMC sections with Scrvie tags
+        if (blocks.blocks.isEmpty())
+            return content; // no changes required
+        byte[] content2 = new byte[content.length - blocks.getTotalLength()];
+        int j = 0, ii = 0;
+        for (BlockFinder.BlockIndex i: blocks.blocks) {
+            final int len = i.start - ii;
+            System.arraycopy(content, ii, content2, j, len);
+            j += len;
+            ii = i.end;
+        }
+        System.arraycopy(content, ii, content2, j, content.length - ii);
+
+        // Update resource refs
+        cb.clear();
+        cb.put("Do".getBytes(), res);
+        res.add = false;
+        search(content2, cb);
+
+        // Remove resources not referenced any more in new content
+        final PdfDictionary xobj = (null != resources) ? resources.getAsDict(PdfName.XOBJECT) : null;
+        if (null != xobj) {
+            for (String r: res.refs) {
+                xobj.remove(new PdfName(r.substring(1)));
+            }
+        }
+        return content2;
+    }
 }
